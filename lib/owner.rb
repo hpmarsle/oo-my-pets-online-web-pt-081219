7359e74213f8d53754bb57589a3c7e96d587bf77
Mars Le <hp.mars.le@gmail.com>
2019-09-10 01:36:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,66 @@
+require "pry"
 class Owner
-  # code goes here
+  attr_accessor :cats, :dogs
+  attr_reader :species, :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    @cats = []
+    @dogs = [] 
+    @@all << self 
+  end 
+  
+  def self.count
+    @@all.count
+  end
+  
+  def say_species 
+    "I am a #{@species}."
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.reset_all
+    @@all.clear
+  end 
+  
+   def buy_cat(name) 
+    Cat.new(name,self)
+   end    
+   
+   def buy_dog(name) 
+    Dog.new(name,self)
+   end 
+   
+   def walk_dogs
+     @dogs.each{|dog|dog.mood = "happy"}
+   end 
+   def feed_cats
+     @cats.each{|cat|cat.mood = "happy"}
+   end 
+   def sell_pets
+     @dogs.each do |dog|
+       dog.mood = "nervous"
+       dog.owner = ""
+     end     
+     @cats.each do |cat|
+       cat.mood = "nervous"
+       cat.owner = ""
+     end 
+   end 
+ 
 end
+
+      
+    # bought_cat = Cat.all.find{|cat|cat.name == cat_name}
+    # prev_owner = self.class.all.find{|owner|owner.name == bought_cat.owner}
+     
+    # #take away the cat instance from the previous owner instance 
+    # prev_owner.cats.delete(bought_cat)   
+     
+    # bought_cat.owner = self.name
+    # self.cats << bought_cat  
